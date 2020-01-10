08679866f021fea33f26ee42c369cbba110c8533
rpupunu <rpupunu@gmail.com>
2020-01-09 19:49:56 -0700

    fixed the basic functionality of the game, just need to clean up the css and swap out the characters

--- src/App.js
+++ src/App.js
@@ -1,35 +1,103 @@
 import React, { Component } from "react";
-import FriendCard from "./components/picsCard";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
 import friends from "./friends.json";
+import update from "immutability-helper";
+import PicsCard from "./components/PicsCard";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
+    topScore: 0,
+    score: 0,
+    headerText: "Don't click on the same image twice!",
     friends
+  }
+
+  handleClick = friendsID => {
+    console.log(friendsID);
+
+    let friendArr = this.state.friends;
+
+    for(let i = 0; i < friendArr.length; i++) {
+
+      if(friendsID === friendArr[i].id) {
+
+        if(friendArr[i].clicked === false){
+
+          this.setState({
+            score: this.state.score +1,
+            friends: update(this.state.friends, {[i]: {clicked: {$set: true }}}),
+            headerText: "You guessed correctly!"
+          })
+
+        } else {
+
+          if(this.state.score > this.state.topScore) {
+            this.setState({
+              topScore: this.state.score,
+              score: 0,
+              headerText: "Wrong! You guessed incorrectly",
+              friends
+            },
+            () => {
+              console.log('Updated new top score!')
+            });
+
+          }
+
+          if(this.state.score <= this.state.topScore) {
+            this.setState({
+              score: 0,
+              headerText: "You guessed wrong!",
+              friends
+            },
+
+            () => {
+              console.log('Reset the game, you did not beat the top score!')
+            });
+          }
+
+        }
+      }
+    }
+
   };
+
+  shuffleArray = array => {
+    let i = array.length - 1;
+    for(; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
 
   
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
-    return (
+  
+
+      let characterCards = this.state.friends.map((friends) =>
+        <PicsCard
+          name = {friends.name}
+          image = {friends.image}
+          key = {friends.id}
+          id = {friends.id}
+          handleClick = {this.handleClick}
+          />
+      )
+
+      let shuffleCards = this.shuffleArray(characterCards)
+
+      return (
       <div>
-        <Header>Pics List</Header>
+        <Header score={this.state.score} topScore = {this.state.topScore} headerText = {this.state.headerText}/>
           <Wrapper>
-            {this.state.friends.map(friend => (
-              <FriendCard
-                removeFriend={this.removeFriend}
-                id={friend.id}
-                key={friend.id}
-                name={friend.name}
-                image={friend.image}
-                occupation={friend.occupation}
-                location={friend.location}
-              />
-            ))}
+            {shuffleCards}
           </Wrapper>
         <Footer/>
       </div>
--- src/components/picsCard/index.js
+++ src/components/picsCard/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./style.css";
 
-function picsCard(props) {
+function PicsCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img alt={props.name} src={props.image} onClick={() => props.handleClick(props.id)} />
       </div>
-     
     </div>
   );
 }
 
-export default picsCard;
+export default PicsCard;
