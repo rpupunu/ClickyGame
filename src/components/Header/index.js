58cbb48469afe19acc1122ef7528082a99cf1866
rpupunu <rpupunu@gmail.com>
2020-01-09 18:39:56 -0700

    Added the props to the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,8 @@ function Header(props) {
     return (
         <div className="header">
             <p id="headerLeft"> Pics Clicky Game </p>
-            <p id="headerRight">Score: </p>
+            <p id="headerMiddle">{props.headerText}</p>
+            <p id="headerRight">Score: {props.score} | Top Score: {props.topScore} </p>
         </div>
     );
 }
