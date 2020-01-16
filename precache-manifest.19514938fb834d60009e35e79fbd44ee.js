c1bc861e8f0b732a09219080e543ebf12eb2e18e
rpupunu <rpupunu@gmail.com>
2020-01-15 21:29:59 -0700

    Updates

---
+++ precache-manifest.19514938fb834d60009e35e79fbd44ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4ce9b74de894181a1617af584f82f7",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "621f59cc76805de95141",
+    "url": "/ClickyGame/static/css/main.c648ee31.chunk.css"
+  },
+  {
+    "revision": "4f497456f2229e7b0b90",
+    "url": "/ClickyGame/static/js/2.9873d65e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.9873d65e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "621f59cc76805de95141",
+    "url": "/ClickyGame/static/js/main.17720867.chunk.js"
+  },
+  {
+    "revision": "d39a9190592d87629923",
+    "url": "/ClickyGame/static/js/runtime-main.68d53d5d.js"
+  }
+]);
