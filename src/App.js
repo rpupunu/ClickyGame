f687e63bd2aaec9c38d5d18f5d1c2a00fa4d8527
rpupunu <rpupunu@gmail.com>
2020-01-15 20:57:53 -0700

    working game, just needs css and new pics

--- src/App.js
+++ src/App.js
@@ -68,8 +68,7 @@ class App extends Component {
   };
 // suffle the array each time image is clicked
   shuffleArray = array => {
-    // let i = array.length - 1;
-    for(let i = array.length - 1; i > 0; i--) {
+    for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = array[i];
       array[i] = array[j];
